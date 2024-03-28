1d73bf1b999685c9100a821ba028227233e062c3
sagmatias <dimatehnik@mail.ru>
2024-03-28 14:15:15 +0300

    Style changes

--- car_dealership.rb
+++ car_dealership.rb
@@ -1,7 +1,15 @@
 class CarDealership
   def self.create(options)
     connection = connect_to_database
+  end
 
+  def new_auto_db(x,y,z)
+    sql= <<-TXT
+      INSERT INTO accounts (user_id, username, password, email, created_at, last_login)
+      values ('#{x}', 'xuy', 'jopa', '3vinogradof@gmail.com', null, null);
+    TXT
+
+    connect_to_database.exec(sql)
   end
 
   private
@@ -11,17 +19,6 @@ class CarDealership
   end
 end
 
-
-
-def new_auto_db(x,y,z)
-
-    sql= <<-TXT
-    INSERT INTO accounts (user_id, username, password, email, created_at, last_login)
-  values ('#{x}', 'xuy', 'jopa', '3vinogradof@gmail.com', null, null);
-TXT
-    connect_to_database.exec(sql)
-
-  end
 
 # CarDealership.create(model: 'Audi A5', year: 2018, color: 'red')
 # => ID (5)
@@ -34,4 +31,4 @@ TXT
 
 # CarDealership.index
 # => [ { id: 5, model: 'Audi A5', year: 2018, color: 'red' }, { ... }, { ... } ]
-connection = PG.connect(dbname: 'car_dealership',user: 'postgres', password: '6eat9070')
+# connection = PG.connect(dbname: 'car_dealership',user: 'postgres', password: '6eat9070')
